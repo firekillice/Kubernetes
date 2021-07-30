6a0d81bd796be271e27a9806599ac2973fc3c7cb
firekillice <firekillice@163.com>
2021-07-30 19:45:16 +0800

    add fiule

--- k3s-install/run.sh
+++ k3s-install/run.sh
@@ -6,6 +6,7 @@ sudo yum install nfs-common
 
 mkdir -p /var/lib/rancher/k3s/agent/images/
 ##次数需要一个镜像，但是github存储有100MB限制，不能上传
+##下载地址：https://github.com/rancher/k3s/releases
 cp ./k3s-airgap-images-amd64.tar.gz /var/lib/rancher/k3s/agent/images/
 
 chmod +x ./install.sh
