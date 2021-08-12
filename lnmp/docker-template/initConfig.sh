93652f7e58846e009a6dab26803c4242f51e9820
bin.wang <bin.wang@capstonegaming.com>
2021-08-13 04:52:36 +0800

    add files

--- lnmp/docker-template/initConfig.sh
+++ lnmp/docker-template/initConfig.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 kubectl delete configmap --all
 
-kubectl create configmap nginx-conf-d-rockmongo-conf --from-file ./conf.d/rock.conf
-kubectl create configmap nginx-conf-d-game-conf --from-file ./conf.d/game.conf
-kubectl create configmap nginx-conf-d-manager-conf --from-file ./conf.d/mgr.conf
-kubectl create configmap nginx-conf-d-upstream-php-conf --from-file ./conf.d/upstream_php-fpm.conf
+kubectl create configmap nginx-conf-d-rock-mongo-conf --from-file ./nginx/conf.d/rock-mongo.conf
+kubectl create configmap nginx-conf-d-game-conf --from-file ./nginx/conf.d/game.conf
+kubectl create configmap nginx-conf-d-manage-conf --from-file ./nginx/conf.d/manage.conf
+kubectl create configmap nginx-conf-d-upstream-php-conf --from-file ./nginx/conf.d/upstream_php-fpm.conf
 
-kubectl create configmap nginx-server-conf --from-file ./server.conf
-kubectl create configmap nginx-nginx-conf --from-file ./nginx.conf
-kubectl create configmap nginx-fcgi-conf --from-file ./fcgi.conf
+kubectl create configmap nginx-server-conf --from-file ./nginx/server.conf
+kubectl create configmap nginx-nginx-conf --from-file ./nginx/nginx.conf
+kubectl create configmap nginx-fcgi-conf --from-file ./nginx/fcgi.conf
+
+kubectl create configmap redis-conf --from-file ./redis/redis.conf
 
 
 kubectl get configmap
