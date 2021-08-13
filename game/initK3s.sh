acca6e0ff5506b022854e913327c647b42759fdb
bin.wang <bin.wang@capstonegaming.com>
2021-08-14 03:19:49 +0800

    update files

--- game/initK3s.sh
+++ game/initK3s.sh
@@ -8,7 +8,7 @@ kubectl create configmap nginx-conf-d-manage-conf --from-file ./nginx/conf/conf.
 kubectl create configmap nginx-conf-d-upstream-php-conf --from-file ./nginx/conf/conf.d/upstream_php-fpm.conf
 
 kubectl create configmap nginx-server-conf --from-file ./nginx/conf/server.conf
-kubectl create configmap nginx-conf-conf --from-file ./nginx/conf/conf.conf
+kubectl create configmap nginx-nginx-conf --from-file ./nginx/conf/nginx.conf
 kubectl create configmap nginx-fcgi-conf --from-file ./nginx/conf/fcgi.conf
 
 kubectl create configmap redis-conf --from-file ./redis/redis.conf
