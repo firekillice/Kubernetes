e2d11523f9fe01edbbb0be192e465546691643d4
firekillice <firekillice@163.com>
2021-08-16 17:42:48 +0800

    update file

--- game/initK3s.sh
+++ game/initK3s.sh
@@ -2,14 +2,20 @@
 
 kubectl delete configmap --all
 
-kubectl create configmap nginx-conf-d-rock-mongo-conf --from-file ./nginx/conf/conf.d/rock-mongo.conf
-kubectl create configmap nginx-conf-d-game-conf --from-file ./nginx/conf/conf.d/game.conf
-kubectl create configmap nginx-conf-d-manage-conf --from-file ./nginx/conf/conf.d/manage.conf
-kubectl create configmap nginx-conf-d-upstream-php-conf --from-file ./nginx/conf/conf.d/upstream_php-fpm.conf
+#kubectl create configmap nginx-conf-d-rock-mongo-conf --from-file ./nginx/conf/conf.d/rock-mongo.conf
+#kubectl create configmap nginx-conf-d-game-conf --from-file ./nginx/conf/conf.d/game.conf
+#kubectl create configmap nginx-conf-d-manage-conf --from-file ./nginx/conf/conf.d/manage.conf
+#kubectl create configmap nginx-conf-d-upstream-php-conf --from-file ./nginx/conf/conf.d/upstream_php-fpm.conf
 
-kubectl create configmap nginx-server-conf --from-file ./nginx/conf/server.conf
-kubectl create configmap nginx-nginx-conf --from-file ./nginx/conf/nginx.conf
-kubectl create configmap nginx-fcgi-conf --from-file ./nginx/conf/fcgi.conf
+
+kubectl create configmap nginx-base-config --from-file=./nginx/conf
+kubectl create configmap nginx-game-config --from-file=./nginx/conf/conf.d
+
+kubectl create configmap server-game-config --from-file=server/config
+
+#kubectl create configmap nginx-server-conf --from-file ./nginx/conf/server.conf
+#kubectl create configmap nginx-nginx-conf --from-file ./nginx/conf/nginx.conf
+#kubectl create configmap nginx-fcgi-conf --from-file ./nginx/conf/fcgi.conf
 
 kubectl create configmap redis-conf --from-file ./redis/redis.conf
 
@@ -17,6 +23,5 @@ kubectl create configmap mongodb-setup-script --from-file ./mongo/setup.sh
 
 kubectl apply -f ./mongo/mongo-cm.yaml
 
-kubectl create configmap server-game-config --from-file=server/config/
 
 kubectl get configmap
