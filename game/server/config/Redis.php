e05bb55c2fa9d8262083f07e6d9e401e1b20a4b3
bin.wang <bin.wang@capstonegaming.com>
2021-08-17 20:52:06 +0800

    update files

--- game/server/config/Redis.php
+++ game/server/config/Redis.php
@@ -4,47 +4,47 @@ return array (
   'password' => '4rT35Ker4m',
   'cache' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'guild' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'push' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'cwar' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'log' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'static' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'seq' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'device' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
   'update' => 
   array (
-    'host' => 'redis-service.default.svc.cluster.local',
+    'host' => 'svc-redis.default.svc.cluster.local',
     'port' => '7478',
   ),
 );
