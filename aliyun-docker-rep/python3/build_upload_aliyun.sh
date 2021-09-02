c7739cdfadb9e5fbe1123ab5ec0b9bf622bc7009
bin.wang <bin.wang@capstonegaming.com>
2021-09-02 14:59:51 +0800

    update

--- aliyun-docker-rep/python3/build_upload_aliyun.sh
+++ aliyun-docker-rep/python3/build_upload_aliyun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REP_CLOUD=registry.cn-hangzhou.aliyuncs.com/sandstone/php:v-game-1.0.0
-docker build -t php-k3s:1.0.0 .
+REP_CLOUD=registry.cn-hangzhou.aliyuncs.com/sandstone/python3:v-game-1.0.0
+docker build -t python3-k3s:1.0.0 .
 
 docker login --username=wang70bin@163.com  --password=123qweasd registry.cn-hangzhou.aliyuncs.com
 
-docker tag php-k3s:1.0.0 $REP_CLOUD
+docker tag python3-k3s:1.0.0 $REP_CLOUD
 
 docker push $REP_CLOUD
--- aliyun-docker-rep/python3/download_from_aliyun.sh
+++ aliyun-docker-rep/python3/download_from_aliyun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REP_CLOUD=registry.cn-hangzhou.aliyuncs.com/sandstone/php:v-game-1.0.0
+REP_CLOUD=registry.cn-hangzhou.aliyuncs.com/sandstone/python3:v-game-1.0.0
 
 docker login --username=wang70bin@163.com --password=123qweasd registry.cn-hangzhou.aliyuncs.com
 
